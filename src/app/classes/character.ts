d64632634fa5ec338ea42e27461000f654728b99
shikari\rehan <rehanyousaf147@gmail.com>
2018-09-28 02:03:00 +0500

    Using Directives
    Example uses of 'Attribute Directives' and 'Structural Directives' added

--- src/app/classes/character.ts
+++ src/app/classes/character.ts
@@ -2,4 +2,5 @@ export class Character {
   name: string;
   height: number;
   gender: string;
+  eye_color: string;
 }
--- src/app/classes/data-characters.ts
+++ src/app/classes/data-characters.ts
@@ -4,26 +4,31 @@ export const CHARACTERS: Character[] = [
   {
     "name": "Anakin Skywalker",
     "height": 1.88,
-    "gender": "male"
+    "gender": "male",
+    "eye_color": "blue"
   },
   {
     "name": "Han Solo",
     "height": 1.8,
-    "gender": "male"
+    "gender": "male",
+    "eye_color": "red"
   },
   {
     "name": "Luke Skywalker",
     "height": 1.72,
-    "gender": "male"
+    "gender": "male",
+    "eye_color": "yellow"
   },
   {
     "name": "Obi-Wan Kenobi",
     "height": 1.82,
-    "gender": "male"
+    "gender": "male",
+    "eye_color": "blue"
   },
   {
     "name": "Kylo Ren",
     "height": 1.9,
-    "gender": "male"
+    "gender": "male",
+    "eye_color": "black"
   }
 ];
